589d0bc77ab8cf84dc5109fc4ffffa4c71cefc97
Mark <github@msss.nl>
2019-10-30 22:53:04 +0100

    Registratie enzo

--- src/main/java/nl/kvk/bitemporal3/Classes.java
+++ src/main/java/nl/kvk/bitemporal3/Classes.java
@@ -2,7 +2,65 @@ package nl.kvk.bitemporal3;
 
 //import java.lang.annotation.*;
 
-public class EntiteitBaseclass
+import java.time.*;
+
+public abstract class FormeelObject
+{
+    enum FormeelObjectType{Registratie, Correctie};
+    FormeelObjectType type; //het type
+
+    FormeelObject[] correcties; //de correcties die er op dit object zijn geweest
+
+    Doorhaling doorhaling; // optioneel
+
+    // indien een registratie
+    LocalDateTime opvoertijdstip; // afgeleid van Registratie: als het een registratie is
+    LocalDateTime afvoertijdstip; // afgeleid van Registratie: optioneel als het een registratie is
+
+    // indien een correctie
+    LocalDateTime correctietijdstip; // afgeleid van Registratie: als het een correctie is
+
+    Registratie registratie;
+}
+
+public class Doorhaling
+{
+    LocalDateTime doorhaaltijdstip; // afgeleid van Gebeurtenis: wanneer was deze doorhaling
+    Registratie registratie;
+}
+
+//eigenlijk de Gebeurtenis
+public class Registratie
+{
+    String gebeurtenisnummer;
+}
+
+
+public abstract class MaterieelObject
+{
+    Aanvang aanvang;
+    Einde einde;
+}
+
+public class Aanvang extends FormeelObject
+{
+    DatumIncompleet datum;
+}
+
+public class Einde extends FormeelObject
+{
+    DatumIncompleet datum;
+}
+
+public class DatumIncompleet
+{
+    int dag;
+    int maand;
+    int jaar;
+}
+
+
+public class EntiteitBaseclass extends FormeelObject
 {
     Entiteitgegevensobject gegevens;
 }
